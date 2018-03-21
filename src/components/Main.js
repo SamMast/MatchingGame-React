d11efa9cd472f9a3ad06f961db094fac32985375
Samuel Mast <samuel.a.mast@gmail.com>
2018-03-20 20:45:57 -0600

    updates to add shuffle function, need to test

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,21 +12,40 @@ class Main extends React.Component {
 
 
   //check function
-  	//if clicked id is in the array -> filter out of array and add a point
-  	//if points > current top score, then update top score to equal current score
-  	//then reshuffle items randomly on page
-  	//else if clicked id is not in the array -> reset game/reshuffle
   check = id => {
+    //if clicked id is in the optionsLeft object -> filter item with that id out of object and add a point
   	if (this.optionsLeft.includes(id)) {
   		this.optionsLeft.filter(option => this.id !== id);
 
       this.setState({ score: this.state.score + 1 });
-
-  		if (this.score > this.topScore) {
-			 this.setState({ topScore: this.state.score });
-  		}
       
-  	}
+      //if points > current top score, then update top score to equal current score
+  		if (this.score > this.topScore) {
+			  this.setState({ topScore: this.state.score });
+  		}
+
+      //then reshuffle items randomly on page
+        this.shuffle();
+  	} else {
+    //else if clicked id is not in the optionsLeft object -> reset game states and reshuffle
+      this.shuffle();
+
+      this.setState({
+        score: 0,
+        optionsLeft: options
+      });
+   
+    }
+  }
+
+  //shuffle function
+  shuffle = () => {
+    for (let i = options.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return options;
+
   }
 
 
@@ -47,16 +66,16 @@ class Main extends React.Component {
 	    	</header>
 
   			<main className="container">
-        {this.state.optionsLeft.map(item => (
-          <div 
-            className="list-group-item"
-            key={item.id}
-            id={item.id}
-            onClick={this.check}
-          >
-            <img src={item.image}/>
-          </div>
-        ))}
+          {this.state.optionsLeft.map(item => (
+            <div 
+              className="list-group-item"
+              key={item.id}
+              id={item.id}
+              onClick={this.check}
+            >
+              <img src={item.image}/>
+            </div>
+          ))}
   			</main>
 
   			<footer className="footer ">
@@ -65,6 +84,7 @@ class Main extends React.Component {
 		  </div>
     );
   }
+
 }
 
 export default Main;
